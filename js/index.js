e90a4558dc4ae63df8afa7086f6cb6a06eb5deac
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-01-21 22:39:16 +0100

    changess

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 `use strict`
 
-// presentation animation
+// presentation animation (funciones de animacion de carga de inicio de la pagina)
 
 const presentationTexts = document.querySelectorAll(`.Presentation-wrapper-text`)
 
@@ -31,12 +31,17 @@ window.onload = ()=>{
             presentationHandler.classList.add(`isHidden`)
 
         } , 10000 )
+
+        setTimeout(()=>{
+
+            presentationHandler.classList.add(`isDisplayNone`)
+
+        } , 11000 )
     })
-
-
 }
 
-// Love behaviour
+
+// Love behaviour background (funciones para que la imagen de fondo de la palabra love se mueva junto con el evento scroll)
 
 const loveText = document.querySelector(`.Main-loveDifference`)
 const windowHeight = document.documentElement.scrollHeight - window.innerHeight
@@ -53,7 +58,7 @@ window.addEventListener(`scroll` , ()=>{
 })
 
 
-// text description flower behaviour
+// text description flower behaviour (funciones para hacer ligero efecto parallax en el fondo de los titulares de cada zapatilla a medida que haces scroll)
 
 const flowerBg = document.querySelector(`.Product-img`)
 const flowerBgReverse = document.querySelector(`.Product-img-reverse`)
@@ -72,7 +77,7 @@ window.addEventListener(`scroll` , ()=>{
     console.log(`translateY(calc(${calculoProporcion / 2}px ))`)
 })
 
-// display product 
+// display product (funciones para mostrar los productos)
 
 const legacyWindow = document.querySelector(`.Legacy`)
 const oneWindow = document.querySelector(`.One`)
