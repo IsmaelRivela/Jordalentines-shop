2bf9b0cd0ad464dacc76a79b1fabfe33282d9a17
IsmaelRivDev <146744081+IsmaelRivela@users.noreply.github.com>
2024-02-13 15:42:07 +0100

    love behaviour changes

--- js/index.js
+++ js/index.js
@@ -43,19 +43,19 @@ window.onload = ()=>{
 
 // Love behaviour background (funciones para que la imagen de fondo de la palabra love se mueva junto con el evento scroll)
 
-const loveText = document.querySelector(`.Main-loveDifference`)
-const windowHeight = document.documentElement.scrollHeight - window.innerHeight
+// const loveText = document.querySelector(`.Main-loveDifference`)
+// const windowHeight = document.documentElement.scrollHeight - window.innerHeight
 
-window.addEventListener(`scroll` , ()=>{
-    let scrollY = window.scrollY
-    let calculoProporcion = Math.floor((100 * scrollY) / windowHeight)
+// window.addEventListener(`scroll` , ()=>{
+//     let scrollY = window.scrollY
+//     let calculoProporcion = Math.floor((100 * scrollY) / windowHeight)
     
     
-    loveText.style.transform = `translateY(calc(${calculoProporcion}% / 2))`
+//     loveText.style.transform = `translateY(calc(${calculoProporcion}% / 2))`
     
 
-    console.log(calculoProporcion)
-})
+//     console.log(calculoProporcion)
+// })
 
 
 // text description flower behaviour (funciones para hacer ligero efecto parallax en el fondo de los titulares de cada zapatilla a medida que haces scroll)
